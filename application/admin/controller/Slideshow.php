ac4ed36984ec0b1f364d8dfade5bf221da4f7a67
wybzx <2625670386@qq.com>
2018-09-09 12:54:39 +0800

    no commit message

--- application/admin/controller/Slideshow.php
+++ application/admin/controller/Slideshow.php
@@ -11,6 +11,23 @@ class Slideshow extends Index
         if (!$this->accountok()) {
             $this->redirect(url('__PUBLIC__/index.php/admin/index/index'));
         }
+
+             $files = request()->file('image');
+
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            if ($files) {
+            	$num=count($files);
+            	for($i=0;$i<$num;$i++){
+                $info = $files[$i]->rule('uniqid')->move(ROOT_PATH . 'public' . DS . 'static'. DS . 'Sowing',$i+1);
+                if ($info) {
+                
+                    $filepath = $info->getSaveName();
+                } else {
+                    // 上传失败获取错误信息
+                    echo $file->getError();
+                }
+            }
+}
         return $this->fetch();
     }
 }
