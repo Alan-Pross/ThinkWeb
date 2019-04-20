1b8418045fa06ce6ed55830298ee05961bf25a4f
Pro.§ <1320722+pross@users.noreply.gitee.com>
2019-04-21 00:29:05 +0800

    thinkphp框架-重大安全性更新补丁 5.0.24

--- thinkphp/library/think/Request.php
+++ thinkphp/library/think/Request.php
@@ -504,9 +504,15 @@ class Request
             // 获取原始请求类型
             return IS_CLI ? 'GET' : (isset($this->server['REQUEST_METHOD']) ? $this->server['REQUEST_METHOD'] : $_SERVER['REQUEST_METHOD']);
         } elseif (!$this->method) {
-            if (isset($_POST[Config::get('var_method')])) {
-                $this->method = strtoupper($_POST[Config::get('var_method')]);
-                $this->{$this->method}($_POST);
+            if (isset($_POST[Config::get('var_method')])) { 
+			$method = strtoupper($_POST[Config::get('var_method')]);
+			if (in_array($method, ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'])) {
+   				$this->method = $method;
+   				$this->{$this->method}($_POST);
+    		} else {
+    			$this->method = 'POST';
+     		}
+			unset($_POST[Config::get('var_method')]);
             } elseif (isset($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'])) {
                 $this->method = strtoupper($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE']);
             } else {
